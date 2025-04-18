b99581af50bf6ca8832c66fbc55562db89870492
yigesong <yigesong@hengtiansoft.com>
2025-04-18 20:34:15 +0800

    import fix

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://your-backend.com',
+        target: 'http://localhost:8080',
         changeOrigin: true
       }
     }
